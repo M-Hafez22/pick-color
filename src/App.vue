e258ece0d698a648cc80f7b6677c91d546c3eb02
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-17 04:50:32 +0200

    💄 Arrange the colors code

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,24 @@ const store = useColorStore();
     :style="{ color: store.color.text, background: store.color.background }"
   >
     <div class="colors">
-      <p>The background Color: {{ store.color.background }}</p>
-      <p>The Text Color: {{ store.color.text }}</p>
-      <p>The Accent Color: {{ store.color.accent }}</p>
+      <p>
+        Background Color:
+        <span :style="{ boxShadow: `0 0 10px ${store.color.accent}` }">{{
+          store.color.background
+        }}</span>
+      </p>
+      <p>
+        Text Color:
+        <span :style="{ boxShadow: `0 0 8px ${store.color.accent}` }">{{
+          store.color.text
+        }}</span>
+      </p>
+      <p>
+        Accent Color:
+        <span :style="{ boxShadow: `0 0 8px ${store.color.accent}` }">{{
+          store.color.accent
+        }}</span>
+      </p>
     </div>
     <PickColor />
   </div>
@@ -27,5 +42,21 @@ const store = useColorStore();
   padding: 0;
   display: flex;
   flex-direction: column;
+}
+.colors {
+  margin: 0.5em;
+  display: flex;
+}
+
+.app p {
+  font-size: 1.5em;
+  padding: 0.25em;
+  margin: 0.25em;
+  width: fit-content;
+}
+span {
+  padding: inherit;
+  box-shadow: 0px 0px 8px;
+  border-radius: 0.3em;
 }
 </style>
