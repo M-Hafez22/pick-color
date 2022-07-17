81e0332254caf4c14cae5cb938ae6eb3a5c7ce57
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-17 04:22:17 +0200

    ✅  Get colors from Store

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
 import PickColor from "./components/PickColor.vue";
+import { useColorStore } from "./stores/color";
+
+const store = useColorStore();
 </script>
 
 <template>
+  <div>
+    <p>The background Color: {{ store.color.background }}</p>
+    <p>The Text Color: {{ store.color.text }}</p>
+    <p>The Accent Color: {{ store.color.accent }}</p>
+  </div>
   <PickColor />
 </template>
 
