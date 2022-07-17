5ee5f7db12bb7aacd3cf76198ae98ef29359d194
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-17 04:53:55 +0200

    Update the test to the new UI

--- src/App.spec.js
+++ src/App.spec.js
@@ -15,10 +15,13 @@ const store = useColorStore()
 
 describe('App', () => {
   it('Get text color from pinia', () => {
-    expect(wrapper.text()).toContain(`The Text Color: ${store.color.text}`)
+    expect(wrapper.text()).toContain(`Text Color: ${store.color.text}`)
   })
   it('Get background Color from pinia', () => {
-    expect(wrapper.text()).toContain(`The background Color: ${store.color.background}`)
+    expect(wrapper.text()).toContain(`Background Color: ${store.color.background}`)
+  })
+  it('Get Accent Color from pinia', () => {
+    expect(wrapper.text()).toContain(`Accent Color: ${store.color.accent}`)
   })
 })
 
