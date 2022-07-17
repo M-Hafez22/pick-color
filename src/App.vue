484e88078ac5c93e01be900674959aea1e9c8b40
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-17 04:36:28 +0200

    💄 Make the app component full screen

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,26 @@ const store = useColorStore();
 </script>
 
 <template>
-  <div>
-    <p>The background Color: {{ store.color.background }}</p>
-    <p>The Text Color: {{ store.color.text }}</p>
-    <p>The Accent Color: {{ store.color.accent }}</p>
+  <div
+    class="app"
+    :style="{ color: store.color.text, background: store.color.background }"
+  >
+    <div>
+      <p>The background Color: {{ store.color.background }}</p>
+      <p>The Text Color: {{ store.color.text }}</p>
+      <p>The Accent Color: {{ store.color.accent }}</p>
+    </div>
+    <PickColor />
   </div>
-  <PickColor />
 </template>
 
 <style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+}
 </style>
