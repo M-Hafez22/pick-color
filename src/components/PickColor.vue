4944bf6b0db5d9b08e75921fc490c185c8ec528a
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-17 04:14:13 +0200

    ✅ Renders labels

--- src/components/PickColor.vue
+++ src/components/PickColor.vue
@@ -1,13 +1,20 @@
 <script setup>
-
 </script>
 
 <template>
   <div>
-    Pick Color
+    <label>background Color:</label>
+    <input type="color" />
+  </div>
+  <div>
+    <label>Text Color:</label>
+    <input type="color" />
+  </div>
+  <div>
+    <label>Accent Color:</label>
+    <input type="color" />
   </div>
 </template>
 
 <style>
-
 </style>
