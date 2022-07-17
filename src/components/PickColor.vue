6388626b9745e2827aebd587636da6a7346a3f71
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-17 04:19:18 +0200

    ✅ Get colors from Store

--- src/components/PickColor.vue
+++ src/components/PickColor.vue
@@ -1,18 +1,27 @@
 <script setup>
+import { ref } from "@vue/reactivity";
+import { useColorStore } from "../stores/color";
+
+const store = useColorStore();
+
+const bgcolor = ref(store.color.background);
+const textcolor = ref(store.color.text);
+const accentcolor = ref(store.color.accent);
 </script>
+
 
 <template>
   <div>
     <label>background Color:</label>
-    <input type="color" />
+    <input type="color" v-model="bgcolor"/>
   </div>
   <div>
     <label>Text Color:</label>
-    <input type="color" />
+    <input type="color" v-model="textcolor"/>
   </div>
   <div>
     <label>Accent Color:</label>
-    <input type="color" />
+    <input type="color" v-model="accentcolor"/>
   </div>
 </template>
 
